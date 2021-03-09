04e903914ee00e52d2749d7c959803c494c34599
Connor Kalahiki <connorkalahiki@gmail.com>
2021-03-09 12:31:33 -0800

    app update commit

---
+++ client/src/Components/Weather.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const Weather = (props) => {
+    console.log(props.latitude, props.longitude);
+
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Weather;
