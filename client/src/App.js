e32d5d9349725f605f8150f41210c95c58ddec69
Connor Kalahiki <connorkalahiki@gmail.com>
2021-03-10 08:08:06 -0800

    weather component and app.js edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,46 @@
 import './App.css';
 
-import Nasa from './Components/Nasa'
+// import Nasa from './Components/Nasa'
 import Weather from './Components/Weather';
 import React, {useState, useEffect} from 'react';
+import {Card, CardBody} from 'reactstrap';
 
 
 
 function App() {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
-  
+
+
   //grabbing lattitude/longitude
   const successfulCoord = (obj) => {
-      setLatitude(Math.round(obj.coords.latitude));
-      setLongitude(Math.round(obj.coords.longitude));
+      setLatitude(obj.coords.latitude);
+      setLongitude(obj.coords.longitude);
   }
 
   const failedCoord = (obj) => {
       console.log('User did not allow location services on browser.');
   }
 
-  window.navigator.geolocation.getCurrentPosition(successfulCoord, failedCoord);
+  //place in a useEffect, or it will continue firing
+  useEffect(() => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successfulCoord, failedCoord);
+    }
+  }, [])
 
   return (
     <div className="App">
-      <Nasa /> 
-      <Weather latitude={latitude} longitude={longitude}/>
+      <br/>
+      <h1>24-Hour React App</h1>
+      <br/>
+      <br/>
+      <Card className='mainBody'>
+        <CardBody >
+          {/* <Nasa /> */}
+          <Weather latitude={latitude} longitude={longitude}/>
+        </CardBody>
+      </Card>
     </div>
   );
 }
