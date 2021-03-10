96c3eedcf0cd556fd5604240b3f944a894bfbaba
NHUTTON <nickolashutton@gmail.com>
2021-03-10 11:10:04 -0500

    pushing image

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
-
 import Nasa from './Components/Nasa'
-import Weather from './Components/Weather';
+// import Weather from './Components/Weather';
 import React, {useState, useEffect} from 'react';
 
 
-
 function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
   //grabbing lattitude/longitude
   const successfulCoord = (obj) => {
-      setLatitude(Math.round(obj.coords.latitude));
-      setLongitude(Math.round(obj.coords.longitude));
+      setLatitude(obj.coords.latitude);
+      setLongitude(obj.coords.longitude);
   }
-
   const failedCoord = (obj) => {
       console.log('User did not allow location services on browser.');
   }
-
-  window.navigator.geolocation.getCurrentPosition(successfulCoord, failedCoord);
+  //place in a useEffect, or it will continue firing
+  useEffect(() => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successfulCoord, failedCoord);
+  }
+  }, [])
 
   return (
-    <div className="App">
-      <Nasa /> 
-      <Weather latitude={latitude} longitude={longitude}/>
-    </div>
-  );
+  <div>
+       <Nasa latitude={latitude} longitude={longitude} /> 
+    {/* <Weather latitude={latitude} longitude={longitude}/> */}
+ </div>
+  )
 }
 
 export default App;
--- client/src/Components/Nasa.js
+++ client/src/Components/Nasa.js
@@ -1,21 +1,42 @@
 import React, {useState, useEffect} from 'react';
+import {Card, CardBody, CardTitle, CardSubtitle} from  'reactstrap';
 
 const Nasa = (props) => {
-    const baseUrl = 'https://api.nasa.gov/planetary/earth/imagery?lon=-95.33&lat=29.78&date=2020-08-02'
+    const [image, setImage] = useState('');
+    const baseUrl = 'https://api.nasa.gov/planetary/earth/assets'
     const key = 'IXEgq2oN9iMcf1dNi08UjQzh78tNNE8TvdLoajZJ'
-    
-    const fetchResults = () => {
-    fetch(`${baseUrl}&api_key=${key}`)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    } 
-    fetchResults()
 
+
+    const fetchResults = () => {
+    const long = props.longitude;
+    const lat = props.latitude;
+
+    if (props.latitude !== '' && props.longitude !== '') {
+        fetch(`${baseUrl}?lon=${long.toFixed(2)}&lat=${lat.toFixed(2)}&&dim=0.280&api_key=${key}`)
+        .then(res => res.json())
+        .then(data => {
+          setImage(data.url)
+        })
+      }
+  }
+
+    useEffect(() => {
+        fetchResults();
+    }, [props.latitude, props.longitude])
+    
     return (
-        <div>
-            <p>Test</p>
-        </div>
+<div>
+      <Card>
+      <img width="50%" src={image} alt="Card image cap" />
+        <CardBody>
+        <CardTitle tag="h5">This is a satelite image of your current location</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">-Image quality is dependent on your location</CardSubtitle>
+        </CardBody>
+      </Card>
+    </div>
     )
 }
+
+
 
 export default Nasa;
