47baa8f014daacef094c2789895fae2acfaf94c9
NHUTTON <nickolashutton@gmail.com>
2021-03-10 11:49:37 -0500

    final commit

--- client/src/Components/Nasa.js
+++ client/src/Components/Nasa.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Card, CardBody, CardTitle, CardSubtitle} from  'reactstrap';
+import {Card, CardBody, CardTitle, CardSubtitle, CardImg} from  'reactstrap';
 
 const Nasa = (props) => {
     const [image, setImage] = useState('');
@@ -27,7 +27,7 @@ const Nasa = (props) => {
     return (
     <div>
       <Card>
-      <img width="50%" src={image} alt="Card image cap" />
+      <CardImg src={image} alt="Card image cap" />
         <CardBody>
         <CardTitle tag="h5">This is a satelite image of your current location</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">-Image quality is dependent on your location</CardSubtitle>
